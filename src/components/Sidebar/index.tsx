6a9981b2e23f5ad075d39773562f3808f82df75b
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-07-08 14:13:28 -0300

    feat: finished website

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
 import {
   EnvelopeSimple,
   GitBranch,
   LinkedinLogo,
   MapPin,
+  Phone,
   Suitcase,
 } from "phosphor-react";
 
@@ -10,12 +13,20 @@ export const Sidebar = () => {
   return (
     <aside className="w-[348px] h-[100%]">
       <div className="w-[348px] h-[292px] bg-[#302F3D] mb-8 flex flex-col items-center justify-center rounded-2xl">
-        <div className="w-[100px] h-[100px] rounded-full bg-slate-400 mb-4 border-2 border-[#00FF00]"></div>
+        <div className="w-[100px] h-[100px] rounded-full bg-slate-400 mb-4 border-2 border-[#00FF00]">
+          <Image
+            src="/img/foto.jpeg"
+            alt="Luís Gabriel Marchió Batista"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+        </div>
         <h1 className="text-[#837E9F] text-[20px] font-bold mb-1">
           Luís Gabriel Marchió Batista
         </h1>
         <p className="text-[#837E9F] text-[15px] font-light">
-          Desenvolvedor full-stack
+          Desenvolvedor full-stack | React.js | Node.js
         </p>
       </div>
 
@@ -23,7 +34,7 @@ export const Sidebar = () => {
         <ul>
           <li className="flex items-center mb-4">
             <MapPin size={32} />
-            <span className="pl-3">Brasil</span>
+            <span className="pl-3">Mineiros - GO | Brasil</span>
           </li>
           <li className="flex items-center mb-4">
             <Suitcase size={32} />
@@ -31,15 +42,27 @@ export const Sidebar = () => {
           </li>
           <li className="flex items-center mb-4">
             <GitBranch size={32} />
-            <span className="pl-3">LuisMarchio03</span>
+            <Link href="https://github.com/LuisMarchio03">
+              <a target="_blank" className="pl-3">
+                LuisMarchio03
+              </a>
+            </Link>
           </li>
           <li className="flex items-center mb-4">
             <LinkedinLogo size={32} />
-            <span className="pl-3">Luís Gabriel Marchió Batista</span>
+            <Link href="https://www.linkedin.com/in/lu%C3%ADs-gabriel-marchi%C3%B3-batista-a0aa64206/">
+              <a target="_blank" className="pl-3">
+                Luís Gabriel Marchió Batista
+              </a>
+            </Link>
           </li>
-          <li className="flex items-center">
+          <li className="flex items-center mb-4">
             <EnvelopeSimple size={32} />
             <span className="pl-3">luisgabrielmarchio75@gmail.com</span>
+          </li>
+          <li className="flex items-center">
+            <Phone size={32} />
+            <span className="pl-3">(64) 9 9991-8525</span>
           </li>
         </ul>
       </div>
@@ -56,19 +79,39 @@ export const Sidebar = () => {
             Javascript
           </div>
           <div className="bg-[#896CEB] text-[#E1E1E6] w-[100px] p-1 rounded-full flex items-center justify-center mr-1 mb-3">
-            Javascript
+            Typescript
           </div>
           <div className="bg-[#896CEB] text-[#E1E1E6] w-[100px] p-1 rounded-full flex items-center justify-center mr-1 mb-3">
-            Javascript
+            React.js
           </div>
           <div className="bg-[#896CEB] text-[#E1E1E6] w-[100px] p-1 rounded-full flex items-center justify-center mr-1 mb-3">
-            Javascript
+            Next.js
           </div>
           <div className="bg-[#896CEB] text-[#E1E1E6] w-[100px] p-1 rounded-full flex items-center justify-center mr-1 mb-3">
-            Javascript
+            Node.js
           </div>
           <div className="bg-[#896CEB] text-[#E1E1E6] w-[100px] p-1 rounded-full flex items-center justify-center mr-1 mb-3">
-            Javascript
+            Nest.js
+          </div>
+        </div>
+      </div>
+
+      <div className="w-[348px] pt-8 pb-8 bg-[#302F3D] text-[#837E9F]  mb-8 flex flex-col justify-center items-center rounded-2xl">
+        <div className="w-full mb-6">
+          <h2 className="text-[#837E9F] text-[20px] font-bold ml-5">
+            Estudos e interesses
+          </h2>
+        </div>
+
+        <div className="flex flex-wrap justify-center">
+          <div className="bg-[#6A80FF] text-[#E1E1E6] w-[150px] p-1 rounded-full flex items-center justify-center mr-1 mb-3">
+            Go Lang
+          </div>
+          <div className="bg-[#6A80FF] text-[#E1E1E6] w-[150px] p-1 rounded-full flex items-center justify-center mr-1 mb-3">
+            Java
+          </div>
+          <div className="bg-[#6A80FF] text-[#E1E1E6] w-[150px] p-1 rounded-full flex items-center justify-center mr-1 mb-3">
+            Microservices
           </div>
         </div>
       </div>
@@ -82,19 +125,11 @@ export const Sidebar = () => {
           <div className="ml-12">
             <ul className="list-disc">
               <li>
-                <strong>Teste</strong>
-                <p className="ml-2">2019 - Atualmente</p>
-                <p className="ml-2">Community Experience</p>
-              </li>
-              <li>
-                <strong>Teste</strong>
-                <p className="ml-2">2019 - Atualmente</p>
-                <p className="ml-2">Community Experience</p>
-              </li>
-              <li>
-                <strong>Teste</strong>
-                <p className="ml-2">2019 - Atualmente</p>
-                <p className="ml-2">Community Experience</p>
+                <strong>B9 Sistemas</strong>
+                <p className="ml-2">2021 - Atualmente</p>
+                <p className="ml-2">
+                  Desenvolvedor Full-Stack | React.js | Node.js
+                </p>
               </li>
             </ul>
           </div>
@@ -110,24 +145,19 @@ export const Sidebar = () => {
           <div className="ml-12">
             <ul className="list-disc">
               <li>
+                <strong>Unifimes | Mineiros - GO</strong>
+                <p className="ml-2">2022 - 2025</p>
+                <p className="ml-2">Bacharelado em Sistemas de Informação</p>
+              </li>
+              <li>
                 <strong>Rocketseat</strong>
-                <p className="ml-2">2022 - 2022</p>
+                <p className="ml-2">2021 - 2022</p>
                 <p className="ml-2">Trilha Node.js</p>
               </li>
               <li>
                 <strong>Rocketseat</strong>
                 <p className="ml-2">2022 - 2022</p>
-                <p className="ml-2">Trilha Node.js</p>
-              </li>
-              <li>
-                <strong>Rocketseat</strong>
-                <p className="ml-2">2022 - 2022</p>
-                <p className="ml-2">Trilha Node.js</p>
-              </li>
-              <li>
-                <strong>Rocketseat</strong>
-                <p className="ml-2">2022 - 2022</p>
-                <p className="ml-2">Trilha Node.js</p>
+                <p className="ml-2">Trilha React.js</p>
               </li>
             </ul>
           </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
-import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
+import type { NextPage } from "next";
 import { Circle, Folder } from "phosphor-react";
-
 import { Sidebar } from "../components/Sidebar";
 
-import { createClient } from "../services/prismicio";
-
-const Home: NextPage = ({ page }: any) => {
-  // console.log(page?.data?.aboutMe[0]?.text);
-  // console.log(page?.data?.myPhoto?.url);
-
+const Home: NextPage = () => {
   return (
     <>
       <Head>
@@ -19,43 +14,110 @@ const Home: NextPage = ({ page }: any) => {
       <main className="py-8 px-8 flex">
         <Sidebar />
 
-        <section className="w-[calc(100%_-_380px)] ml-12 ">
+        <section className="w-[calc(100%_-_380px)] ml-12 flex flex-col gap-8">
           <>
             <div className="w-[100%] h-[86px] mb-12 bg-[#302F3D] text-[837E9F] text-[20px] font-bold flex items-center pl-6 rounded-2xl">
               Meus Projetos
             </div>
 
             <div className="w-[100%] h-auto flex gap-8">
-              <div className="relative w-[50%] p-8 bg-[#302F3D] text-[837E9F] rounded-xl border-b border-[#00FF00]">
-                <h2 className="text-[20px] font-bold flex items-center gap-2">
-                  <Folder size={32} weight="light" />
-                  Teste Repo
-                </h2>
-                <p className="pb-8 pt-4">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
-                  quos eveniet at ratione quod assumenda architecto, illo modi
-                  iste minima aut, deserunt adipisci possimus error laborum
-                  asperiores sint consectetur nisi.
-                </p>
-                <span className="absolute bottom-5 right-5 flex items-center ">
-                  <Circle size={15} weight="fill" color="red" />
-                  <p className="ml-2">Javascript</p>
-                </span>
-              </div>
+              <Link
+                href="https://github.com/LuisMarchio03/api-ecommerce"
+                target="_blank"
+              >
+                <a
+                  target="_blank"
+                  className="relative w-[50%] p-8 bg-[#302F3D] text-[837E9F] rounded-xl border-b cursor-pointer"
+                >
+                  <h2 className="text-[20px] font-bold flex items-center gap-2">
+                    <Folder size={32} weight="light" />
+                    Api-Ecommerce
+                  </h2>
+                  <p className="pb-8 pt-4">
+                    API E-commerce | Node.js + Express.js + Typescript + TDD +
+                    SOLID + Testes Automatizados + TypeORM + PrismaIO + Nest.js
+                  </p>
+                  <span className="absolute bottom-5 right-5 flex items-center ">
+                    <Circle size={15} weight="fill" color="#7cb803" />
+                    <p className="ml-2">Node.js | Typescript</p>
+                  </span>
+                </a>
+              </Link>
 
-              <div className="relative w-[50%] p-8 bg-[#302F3D] text-[837E9F] rounded-xl border-b border-[#00FF00]">
-                <h2 className=" text-[20px] font-bold">Teste Repo</h2>
-                <p className="pb-8 pt-4">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
-                  quos eveniet at ratione quod assumenda architecto, illo modi
-                  iste minima aut, deserunt adipisci possimus error laborum
-                  asperiores sint consectetur nisi.
-                </p>
-                <span className="absolute bottom-5 right-5 flex items-center ">
-                  <Circle size={15} weight="fill" color="red" />
-                  <p className="ml-2">Javascript</p>
-                </span>
-              </div>
+              <Link
+                href="https://github.com/LuisMarchio03/Feedget"
+                target="_blank"
+              >
+                <a
+                  target="_blank"
+                  className="relative w-[50%] p-8 bg-[#302F3D] text-[837E9F] rounded-xl border-b cursor-pointer"
+                >
+                  <h2 className="text-[20px] font-bold flex items-center gap-2">
+                    <Folder size={32} weight="light" />
+                    Feedget
+                  </h2>
+
+                  <p className="pb-8 pt-4">
+                    Feedget | React.js, Tailwind, Acessibility, Microservices,
+                    Solid, Test-automation, React Native
+                  </p>
+                  <span className="absolute bottom-5 right-5 flex items-center ">
+                    <Circle size={15} weight="fill" color="#2676c6" />
+                    <p className="ml-2">Typescript</p>
+                  </span>
+                </a>
+              </Link>
+            </div>
+
+            <div className="w-[100%] h-auto flex gap-8">
+              <Link
+                href="https://github.com/LuisMarchio03/ignite-rentx"
+                target="_blank"
+              >
+                <a
+                  target="_blank"
+                  className="relative w-[50%] p-8 bg-[#302F3D] text-[837E9F] rounded-xl border-b cursor-pointer"
+                >
+                  <h2 className="text-[20px] font-bold flex items-center gap-2">
+                    <Folder size={32} weight="light" />
+                    Ignite Rentx
+                  </h2>
+                  <p className="pb-8 pt-4">
+                    API Desenvolvida durante o curso Ignite Trilha Node.js |
+                    Node.js + Express.js + Typescript + TDD + SOLID + Testes
+                    Automatizados + TypeORM
+                  </p>
+                  <span className="absolute bottom-5 right-5 flex items-center ">
+                    <Circle size={15} weight="fill" color="#7cb803" />
+                    <p className="ml-2">Node.js | Typescript</p>
+                  </span>
+                </a>
+              </Link>
+
+              <Link
+                href="https://github.com/LuisMarchio03/rocketseat-ignite-lab"
+                target="_blank"
+              >
+                <a
+                  target="_blank"
+                  className="relative w-[50%] p-8 bg-[#302F3D] text-[837E9F] rounded-xl border-b cursor-pointer"
+                >
+                  <h2 className="text-[20px] font-bold flex items-center gap-2">
+                    <Folder size={32} weight="light" />
+                    Ignite Lab
+                  </h2>
+
+                  <p className="pb-8 pt-4">
+                    Projeto completo Desenvolvido durante a maratona Ignite Lab
+                    01 | Nest.js + Microservice + GraphQL + Apollo + Next.js +
+                    Typescript + PrismaIO
+                  </p>
+                  <span className="absolute bottom-5 right-5 flex items-center ">
+                    <Circle size={15} weight="fill" color="#2676c6" />
+                    <p className="ml-2">Typescript</p>
+                  </span>
+                </a>
+              </Link>
             </div>
           </>
 
@@ -64,9 +126,9 @@ const Home: NextPage = ({ page }: any) => {
               Posts Recentes
             </div>
 
-            <div className="w-[100%] h-auto">
+            {/* <div className="w-[100%] h-auto">
               <div className="w-[100%] h-auto flex gap-8">
-                <div className="relative w-[100%] p-8 bg-[#302F3D] text-[837E9F] rounded-xl border-b border-[#00FF00] flex items-center gap-6">
+                <div className="relative w-[100%] p-8 bg-[#302F3D] text-[837E9F] rounded-xl border-b flex items-center gap-6">
                   <div className="w-[150px] h-[150px] rounded-full bg-slate-400 mb-4"></div>
                   <div className="w-[85%]">
                     <h2 className="text-[20px] font-bold flex items-center gap-2">
@@ -91,21 +153,12 @@ const Home: NextPage = ({ page }: any) => {
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
           </>
         </section>
       </main>
     </>
   );
-};
-
-export const getStaticProps: GetStaticProps = async ({ previewData }: any) => {
-  const client = createClient({ previewData });
-  const page = await client.getSingle("portfolio");
-
-  return {
-    props: { page },
-  };
 };
 
 export default Home;
